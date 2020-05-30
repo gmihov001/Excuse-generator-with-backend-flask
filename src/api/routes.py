fc8650f5ee9317dd5b402d0304116ab0d0ff9a80
George Mihov <gmihov001@gmail.com>
2020-05-30 00:45:45 +0000

    Updates

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,6 +1,8 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+import math
+import random
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db
 from api.utils import generate_sitemap
@@ -9,11 +11,23 @@ from api.utils import generate_sitemap
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/excuse', methods=['GET'])
+def handle_excuse():
 
+    who = ["My dog ", "My grandma ", "His turtle ", "My bird "]
+    did = [" ate ", " lost ", " crashed ", " broke "]
+    what = [" my lunch ", " my keys ", " my car ", " my head "]
+    when = [
+        " just when I was about to leave.",
+        " right before you called.",
+        " when I was finishing class.",
+        " during my lunch.",
+        " while I was praying."
+    ]
+
+    excuse = who[math.floor(random.randint(0, len(who)-1))] + did[math.floor(random.randint(0, len(did)-1))] + what[math.floor(random.randint(0, len(what)-1))] + when[math.floor(random.randint(0, len(when)-1))]
     response_body = {
-        "hello": "world"
+        "excuse": excuse
     }
 
     return jsonify(response_body), 200
--- src/front/js/index.js
+++ src/front/js/index.js
@@ -5,6 +5,10 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+window.onload = async function() {
+  const response = await fetch(
+    "https://3000-a1ac14a2-1bbc-4ae5-85a4-8773533b8876.ws-us02.gitpod.io/api/excuse"
+  );
+  let response_object = await response.json();
+  document.querySelector(".alert").innerHTML = response_object.excuse;
 };
